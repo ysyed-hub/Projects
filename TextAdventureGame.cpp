ed387df7eb43cc5c38b8a5ffb1d2af79e8fa9b5d
djysyed <whysigh729@gmail.com>
2020-09-05 13:00:34 -0700

    fixed style

--- TextAdventureGame.cpp
+++ TextAdventureGame.cpp
@@ -1,3 +1,4 @@
+
 //
 //  main.cpp
 //  Text Adventure Game
@@ -7,116 +8,120 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-class Character { // Sorcerer, Wariror, Ranger
+class Character { // Sorcerer, Warrior, Ranger
   
-    int Health;
-    int Attack_Damage;
-    int Magic_Damage;
-    int Gold;
-    int Exp;
-    int Level;
+    int type;
+    int health;
+    int attack_damage;
+    int magic_damage;
+    int gold;
+    int exp;
+    int level;
     
 public:
-    void Show_Stats (void) {
-        cout << "\nLevel: " << Level << endl;
-        cout << "Health: " << Health << endl;
-        cout << "Physical Damage: " << Attack_Damage << endl;
-        cout << "Magic Damage: " << Magic_Damage << endl;
-        cout << "Gold: " << Gold << endl;
-        cout << "Exp: " << Exp << endl;
-
+    int HeroType(void) {return type;}
+    void ShowStats (void) {
+        cout << "\nLevel: " << level << endl;
+        cout << "Health: " << health << endl;
+        cout << "Physical Damage: " << attack_damage << endl;
+        cout << "Magic Damage: " << magic_damage << endl;
+        cout << "Gold: " << gold << endl;
+        cout << "Exp: " << exp << endl;
     }
     
-    Character(int x, int y, int z) {
-        Health = x;
-        Attack_Damage = y;
-        Magic_Damage = z;
-        Gold = 0;
-        Exp = 0;
-        Level = 1;
+    Character(int w, int x, int y, int z) {
+        type = w;
+        health = x;
+        attack_damage = y;
+        magic_damage = z;
+        gold = 0;
+        exp = 0;
+        level = 1;
     }
     
-    int Damage_Upgrade(void) {
-        Gold -= 50;
-        if (Attack_Damage == 0) {
-            Magic_Damage += 20;
-            return Magic_Damage;
+    int DamageUpgrade(void) {
+        gold -= 50;
+        if (attack_damage == 0) {
+            magic_damage += 20;
+            return magic_damage;
         }
-        Attack_Damage += 20;
-        return Attack_Damage;
+        attack_damage += 20;
+        return attack_damage;
     }
     
-    int Health_Upgrade(void) {
-        Health += 40;
-        Gold -= 50;
-        return Health;
+    int HealthUpgrade(void) {
+        health += 40;
+        gold -= 50;
+        return health;
     }
-    int Gold_Count(void) {return Gold;}
-    int Character_Health(void) {return Health;}
-    int Character_Attack(void) {return (Attack_Damage > 0 ? Attack_Damage : Magic_Damage);}
-    int Exp_Gain(int experience_gain) {
-        Exp += experience_gain;
-        return Exp;
+    int GoldCount(void) {return gold;}
+    int CharacterHealth(void) {return health;}
+    int CharacterAttack(void) {return (attack_damage > 0 ? attack_damage : magic_damage);}
+    int ExpGain(int experience_gain) {
+        exp += experience_gain;
+        return exp;
     }
-    int Gold_Gain(int gold_gain) {
-        Gold += gold_gain;
-        return Gold;
+    int GoldGain(int gold_gain) {
+        gold += gold_gain;
+        return gold;
     }
     
-    void Level_Up(int type, int Level_Up_Exp) {
-        if (type == 1) {
-            Health += 25;
-            Magic_Damage += 10;
+    void LevelUp(int type) {
+        double x = 100 * pow(1.1, level);
+        int level_up_exp = x;
+        while (exp > level_up_exp) {
+            if (type == 1) {
+                health += 25;
+                magic_damage += 10;
+            }
+            if (type == 2) {
+                health += 40;
+                attack_damage += 5;
+            }
+            if (type == 3) {
+                health += 20;
+                attack_damage += 12;
+            }
+            exp -= level_up_exp;
+            level++;
+            level_up_exp *= 1.1;
         }
-        if (type == 2) {
-            Health += 40;
-            Attack_Damage += 5;
-        }
-        if (type == 3) {
-            Health += 20;
-            Attack_Damage += 12;
-        }
-        Level++;
-        Exp -= Level_Up_Exp;
+        cout << "\nCongrats! You have leveled up to level " << level << "." << endl;
     }
     
 };
 
-typedef Character *characterptr;
-
 class Enemy { // Goblin, Wolf, Troll, Giant, Dragon
-    int Health;
-    int Attack;
-    int Gold;
-    int Experience;
+    int health;
+    int attack;
+    int gold;
+    int experience;
     
 public:
     Enemy(int x, int y, int z, int w) {
-        Health = x;
-        Attack = y;
-        Gold = z;
-        Experience = w;
+        health = x;
+        attack = y;
+        gold = z;
+        experience = w;
     }
-    int Enemy_Gold(void) {return Gold;}
-    int Enemy_Health(void) {return Health;}
-    int Enemy_Attack(void) {return Attack;}
-    int Enemy_Experience(void) {return Experience;}
+    int EnemyGold(void) {return gold;}
+    int EnemyHealth(void) {return health;}
+    int EnemyAttack(void) {return attack;}
+    int EnemyExperience(void) {return experience;}
 };
 
-typedef Enemy *enemyptr;
-
-
-int introduction(string *p);
-int menu(void);
-void upgrade(characterptr p);
-int upgrade_menu(void);
-bool adventure(characterptr p);
-int adventure_menu(void);
-void battle(characterptr p, Enemy q, string enemy_type);
-bool battle_boss(characterptr p, Enemy q);
+int Introduction(string *p);
+int Menu(void);
+void Upgrade(Character *p);
+int UpgradeMenu(void);
+bool Adventure(Character *p);
+int AdventureMenu(void);
+void Battle(Character *p, Enemy q, string enemy_type);
+bool BattleBoss(Character *p, Enemy q);
 Enemy Goblin(50, 20, 100, 50);
 Enemy Wolf(250, 70, 250, 200);
 Enemy Troll(1000, 200, 800, 500);
@@ -124,59 +129,55 @@ Enemy Giant(5500, 1000, 1500, 1000);
 Enemy Dragon(12000, 2000, 0, 0);
 
 int main(int argc, const char * argv[]) {
-    string Char_Name;
-    string *p = &Char_Name;
-    int Char_Type = introduction(p);
+    string char_name;
+    string *p = &char_name;
+    int char_type = Introduction(p);
     int x, y, z;
-    if (Char_Type == 1) {
+    if (char_type == 1) {
+        
         x = 200;
         y = 0;
         z = 100;
     }
-    if (Char_Type == 2) {
+    if (char_type == 2) {
         x = 250;
         y = 70;
         z = 0;
     }
-    if (Char_Type == 3) {
+    if (char_type == 3) {
         x = 150;
         y = 120;
         z = 0;
     }
     
-    Character User(x, y ,z);
-    characterptr pUser = &User;
-    bool Final_Boss = false; // Final boss has not been defeated
-    bool Leave_Game = false;
+    Character User(char_type, x, y ,z);
+    Character *p_user = &User;
+    bool final_boss = false; // Final boss has not been defeated
+    bool leave_game = false;
     
-    while (Final_Boss == false && Leave_Game == false) {
+    while (final_boss == false && leave_game == false) {
         
-        int User_Choice = menu();
-        switch (User_Choice) {
+        int user_choice = Menu();
+        switch (user_choice) {
             case 1:
-                Final_Boss = adventure(pUser);
+                final_boss = Adventure(p_user);
                 break;
             case 2:
-                upgrade(pUser);
+                Upgrade(p_user);
                 break;
             case 3:
-                User.Show_Stats();
+                User.ShowStats();
                 break;
             case 4:
-                Leave_Game = true;
-                break;
-            default:
-                cout << "Improper input, please enter your choice again: ";
+                leave_game = true;
                 break;
         }
     }
     
-    if (Final_Boss == true) {
+    if (final_boss == true) {
         // ending credits
     }
-    
-    cout << "Thanks for playing!" << endl;
-    
+    cout << "\nThanks for playing!\n" << endl;
     return 0;
 }
 
@@ -186,39 +187,46 @@ int menu(void) {
     cout << "3: Show character stats" << endl;
     cout << "4: Leave game\n" << endl;
     cout << "Enter the number of the option you choose: ";
-    int x;
+    string x;
     cin >> x;
-    return x;
+    while (x != "1" && x != "2" && x != "3" && x != "4") {
+        cout << "\nImproper input, please choose either 1, 2, 3 or 4: ";
+        cin >> x;
+    }
+    if (x == "1")
+        return 1;
+    if (x == "2")
+        return 2;
+    if (x == "3")
+        return 3;
+    return 4;
 }
 
-bool adventure(characterptr p) {
+bool Adventure(Character *p) {
     bool game_complete = false;
-    int adventure_choice = adventure_menu();
-    while (adventure_choice < 1 || adventure_choice > 5) {
-        cout << "Invalid input, please enter a valid input: " << endl;
-        cin >> adventure_choice;
-    }
+    int adventure_choice = AdventureMenu();
     switch (adventure_choice) {
         case 1:
-            battle(p, Goblin, "goblins");
+            Battle(p, Goblin, "goblins");
             break;
         case 2:
-            battle(p, Wolf, "wolves");
+            Battle(p, Wolf, "wolves");
             break;
         case 3:
-            battle(p, Troll, "trolls");
+            Battle(p, Troll, "trolls");
             break;
         case 4:
-            battle(p, Giant, "giant");
+            Battle(p, Giant, "giant");
             break;
         case 5:
-            game_complete = battle_boss(p, Dragon);
+            game_complete = BattleBoss(p, Dragon);
             break;
     }
+    p -> LevelUp(p -> HeroType());
     return game_complete;
 }
 
-int adventure_menu(void) {
+int AdventureMenu(void) {
     cout << "\nWhat type of adventure do you plan on going on?\n" << endl;
     cout << "1) Fight the goblins that reside in the northern caves." << endl;
     cout << "2) Destroy the pack of wolves in the forest" << endl;
@@ -226,52 +234,68 @@ int adventure_menu(void) {
     cout << "4) Challenge a giant." << endl;
     cout << "5) Defeat the great dragon\n" << endl;
     cout << "Enter the number of the option you wish to choose: ";
-    int x;
+    string x;
     cin >> x;
-    return x;
+    while (x != "1" && x != "2" && x != "3" && x != "4" && x != "5") {
+        cout << "\nImproper input, please choose either 1, 2, 3, 4 or 5: ";
+        cin >> x;
+    }
+    if (x == "1")
+        return 1;
+    if (x == "2")
+        return 2;
+    if (x == "3")
+        return 3;
+    if (x == "4")
+        return 4;
+    return 5;
 }
 
-void upgrade(characterptr p) {
-    if (p -> Gold_Count() < 50) {
+void Upgrade(Character *p) {
+    cout << "\n";
+    if (p -> GoldCount() < 50) {
         cout << "Not enough gold to make any upgrades." << endl;
         return;
     }
     int upgrade_choice;
-    while ((upgrade_choice = upgrade_menu()) != 3) {
-        while (upgrade_choice < 1 || upgrade_choice > 3) {
-            cout << "Improper input, please enter your choice again: ";
-            cin >> upgrade_choice;
-        }
+    while ((upgrade_choice = UpgradeMenu()) != 3) {
+        
         if (upgrade_choice == 1) {
-            cout << "Your health has been increased to " << p -> Health_Upgrade() << "." << endl;
+            cout << "Your health has been increased to " << p -> HealthUpgrade() << "." << endl;
         }
         if (upgrade_choice == 2) {
-            cout << "Your health has been increased to " << p -> Damage_Upgrade() << "." << endl;
+            cout << "Your damage has been increased to " << p -> DamageUpgrade() << "." << endl;
         }
-        p -> Gold_Gain(-50);
-        if (p -> Gold_Count() < 50) {
+        p -> GoldGain(-50);
+        if (p -> GoldCount() < 50) {
             cout << "\nNot enough gold to make any additional upgrades." << endl;
             return;
         }
     }
     cout << "Thank you for coming!\n";
-    
-    
 }
 
-int upgrade_menu(void) {
+int UpgradeMenu(void) {
     cout << "You can increase your maximum health by 40 or increase your attack by 20 for 50 gold." << endl;
     cout << "Enter 1 to increase health, 2 to increase attack or 3 to leave: ";
-    int x;
+    string x;
     cin >> x;
-    return x;
+    while (x != "1" && x != "2" && x != "3") {
+        cout << "\nImproper input, please choose either 1, 2, or 3: ";
+        cin >> x;
+    }
+    if (x == "1")
+        return 1;
+    if (x == "2")
+        return 2;
+    return 3;
 }
 
-bool battle_boss(characterptr p, Enemy q) {
-    int user_health = p -> Character_Health();
-    int user_attack = p -> Character_Attack();
-    int enemy_health = q.Enemy_Health();
-    int enemy_attack = q.Enemy_Attack();
+bool BattleBoss(Character *p, Enemy q) {
+    int user_health = p -> CharacterHealth();
+    int user_attack = p -> CharacterAttack();
+    int enemy_health = q.EnemyHealth();
+    int enemy_attack = q.EnemyAttack();
     while (user_health > 0 && enemy_health > 0) {
         user_health -= enemy_attack;
         enemy_health -= user_attack;
@@ -284,13 +308,13 @@ bool battle_boss(characterptr p, Enemy q) {
     return false;
 }
 
-void battle(characterptr p, Enemy q, string enemy_type) {
-    int user_health = p -> Character_Health();
-    int user_attack = p -> Character_Attack();
-    int enemy_health = q.Enemy_Health();
-    int enemy_attack = q.Enemy_Attack();
-    int experience = q.Enemy_Experience();
-    int gold = q.Enemy_Gold();
+void Battle(Character *p, Enemy q, string enemy_type) {
+    int user_health = p -> CharacterHealth();
+    int user_attack = p -> CharacterAttack();
+    int enemy_health = q.EnemyHealth();
+    int enemy_attack = q.EnemyAttack();
+    int experience = q.EnemyExperience();
+    int gold = q.EnemyGold();
     while (user_health > 0 && enemy_health > 0) {
         user_health -= enemy_attack;
         enemy_health -= user_attack;
@@ -298,8 +322,8 @@ void battle(characterptr p, Enemy q, string enemy_type) {
     if (user_health > 0) {
         cout << "\nYou have defeated the " << enemy_type << "!" << endl;
         cout << "You are rewarded " << gold << " gold and " << experience << " experience." << endl;
-        p -> Gold_Gain(gold);
-        p -> Exp_Gain(experience);
+        p -> GoldGain(gold);
+        p -> ExpGain(experience);
         return;
     }
     cout << "\nYou have been defeated by the " << enemy_type << ", please come back once you have become stronger." << endl;
@@ -308,7 +332,7 @@ void battle(characterptr p, Enemy q, string enemy_type) {
 
 
 
-int introduction(string *p) {
+int Introduction(string *p) {
     cout << "Welcome to my text adventure game." << endl;
     cout << "Enter your name: ";
     string s;
@@ -322,12 +346,17 @@ int introduction(string *p) {
     cout << "2. Warrior, HP: 250 + 40 per level, Physical Damage: 70 + 7 per level" << endl;
     cout << "3. Ranger, HP: 150 + 20 per level, Physical Damage: 120 + 12 per level\n" << endl;
     cout << "Enter the number representing your choice of character type: ";
-    int x;
+    string x;
     cin >> x;
-    while (x < 1 || x > 3) {
-        cout << "Improper input, please choose either 1, 2, or 3: ";
+    
+    while (x != "1" && x != "2" && x != "3") {
+        cout << "\nImproper input, please choose either 1, 2, or 3: ";
         cin >> x;
     }
-    return x;
+    if (x == "1")
+        return 1;
+    if (x == "2")
+        return 2;
+    return 3;
     
 }
